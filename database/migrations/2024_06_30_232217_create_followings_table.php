9d4b30305b069334641504584e062486dcc3af34
mmk <mmkkarbalaei@gmail.com>
2024-06-30 23:23:25 +0330

    Create followings table migration

---
+++ database/migrations/2024_06_30_232217_create_followings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('followings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('following_shop_id')->constrained('shops')->onDelete('cascade');
+            $table->timestamp('following_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('followings');
+    }
+};
