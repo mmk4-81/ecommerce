8a30283367a9dc39256e3df8aac6a74b667d2f39
mmk <mmkkarbalaei@gmail.com>
2024-06-30 20:03:28 +0330

    Added avatar, credit, and role fields to users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone', 11);
+            $table->string('avatar')->nullable();
+            $table->decimal('credit', 10, 3)->default(0);
+            $table->enum('role', ['admin', 'user', 'seller'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
