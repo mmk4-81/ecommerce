dc361aee5a62366f9a30e9ac5f434418d557e7ee
mmk <mmkkarbalaei@gmail.com>
2024-06-30 23:45:42 +0330

    Create product_variations table migration

---
+++ database/migrations/2024_06_30_234506_create_product_variations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variations', function (Blueprint $table) {
+            $table->id();
+            $table->string('value')->nullable();
+            $table->decimal('price', 10, 3);
+            $table->integer('quantity')->default(0);
+            $table->foreignId('attribute_id')->constrained('attributes')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variations');
+    }
+};
