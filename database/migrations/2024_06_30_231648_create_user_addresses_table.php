a19da9e7dabb8955a4e9813dbaa224fed7bbdf6e
mmk <mmkkarbalaei@gmail.com>
2024-06-30 23:18:02 +0330

    Create user_addresses table migration

---
+++ database/migrations/2024_06_30_231648_create_user_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('province');
+            $table->string('city');
+            $table->string('title');
+            $table->text('address');
+            $table->string('postal_code', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+};
