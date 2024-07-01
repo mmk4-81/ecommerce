536ba17ac8189762cef4d45bc03f556ef589c112
mmk <mmkkarbalaei@gmail.com>
2024-07-01 15:44:55 +0330

    update model order item

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -20,4 +20,14 @@ class OrderItem extends Model
     {
         return $this->belongsTo(Order::class);
     }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function productVariation()
+    {
+        return $this->belongsTo(ProductVariation::class);
+    }
 }
